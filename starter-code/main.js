c0d573e9191806651e42dff536bb20dd639cad8c
Michael Mahony <michael.mahony@umbc.edu>
2016-06-27 00:57:15 -0400

    Finished section 11 of the fundamentals assignment.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,46 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-if (cardOne === cardTwo) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.")
+var createBoard = function() {
+	var board = document.getElementById("game-board")
+
+	for (var i = 0; i < cards.length; i++) {
+		var newCard = document.createElement("div");
+		newCard.className = "card";
+		newCard.setAttribute('data-card', cards[i]);
+		
+		newCard.addEventListener('click', isTwoCards);
+
+		board.appendChild(newCard);
+	}
+
 }
+
+var isMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again");
+	}
+}
+
+var isTwoCards = function() {
+  // add card to array of cards in play
+  // 'this' hasn't been covered in this prework, but
+  // for now, just know it gives you access to the card the user clicked on
+  cardsInPlay.push(this.getAttribute('data-card'));
+
+  // if you have two cards in play check for a match
+  if (cardsInPlay.length === 2) {
+
+    // pass the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+
+    // clear cards in play array for next try
+    cardsInPlay = [];
+  }
+}
+
+createBoard()
